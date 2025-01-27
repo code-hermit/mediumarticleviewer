012360072721b8a694e968865b60c8676d8858a9
codehermit <codehermit.algo@gmail.com>
2025-01-27 23:22:41 +0530

    added pics

--- src/app/components/medium-feed/medium-feed.component.ts
+++ src/app/components/medium-feed/medium-feed.component.ts
@@ -14,8 +14,21 @@ export class MediumFeedComponent implements OnInit {
   selectedArticle: any | null = null;
   generatedImageUrl: string | null = null;
   username: string = '@kirubakaranrajendran'; // Replace with your Medium username
+  fallbackImage: string = './../../../assets/squareoff.jpg';
 
   constructor(private mediumFeedService: MediumFeedService) {}
+  convertToAlphanumeric(input: string) {
+    return input.replace(/[^a-zA-Z0-9]/g, '');
+  }
+  get_image(title: string) {
+    console.log(this.convertToAlphanumeric(title), 20);
+    return `https://mediummeta.s3.ap-south-1.amazonaws.com/${this.convertToAlphanumeric(
+      title
+    )}.jpg`;
+  }
+  onImageError(event: Event) {
+    (event.target as HTMLImageElement).src = this.fallbackImage;
+  }
 
   async ngOnInit() {
     this.articles = await this.mediumFeedService.fetchMediumFeed(this.username);
